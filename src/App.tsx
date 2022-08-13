d34df318fefe59bac9f5276c8df8b5b7a6e1fb48
Alexandr Shishov <ernesto.chrome@mail.ru>
2022-08-13 11:47:04 +0300

    learned the UseMemo hook; studied the UseMemo hook in conjunction with React.memo;

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import './App.css';
-import { Example } from './components/ReactMemo';
+import { Example } from './components/UseMemo';
+import { UseMemoFactorialCounter } from './components/UseMemo';
 
 function App() {
   return (
     <div className='App'>
+      {/* <UseMemoFactorialCounter /> */}
       <Example />
     </div>
   );
