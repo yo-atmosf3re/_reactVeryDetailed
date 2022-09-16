8f5b9b6cfa473a01e7dec6ca29b5d74872467712
Alexandr Shishov <ernesto.chrome@mail.ru>
2022-09-16 09:48:03 +0300

    I study clearing and resetting the UseEffect on the example of the Counter component;

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,13 @@ import * as React from 'react';
 import { useState } from 'react';
 import './App.css';
 import Clock from './components/Clock/Clock';
+import UseEffectCleanup from './components/useEffectCleanup/useEffectCleanup';
 
 
 function App() {
-
-  const [sw, setSw] = useState(false)
-  const onClickHandler = () => {
-    setSw(!sw)
-  }
-
   return (
     <div>
-      <Clock mode='analog' />
-      <>
-
-      </>
+      <UseEffectCleanup />
     </div>
   );
 }
